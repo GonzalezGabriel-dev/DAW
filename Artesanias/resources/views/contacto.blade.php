55dfc091ee07361cb3571249bacfcfbaa7ad046a
GonzalezGabriel-dev <71902209+GonzalezGabriel-dev@users.noreply.github.com>
2021-03-22 13:34:40 -0700

    Introducción a Bootstrap

--- Artesanias/resources/views/contacto.blade.php
+++ Artesanias/resources/views/contacto.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body style="background-color: {{ $fondo }};">
+<body style="background-color: {{ $fondo }}">
     @for($i=0;$i<$valores;$i++)
         @if($i%2 == 0)
         <h2 style="color: blue"> {{ $nombre }} </h2>
